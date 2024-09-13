fe69f4c823e8696597db007e34d07169f7b3b1ab
Caleb Awe <calebawe0@gmail.com>
2024-09-13 03:05:58 +0000

    Updated file

--- main.c
+++ main.c
@@ -1,7 +1,3 @@
-/*This source code copyrighted by Lazy Foo' Productions (2004-2022)
-and may not be redistributed without written permission.*/
-
-//Using SDL and standard IO
 #include <SDL.h>
 #include <stdio.h>
 #include <stdbool.h>
